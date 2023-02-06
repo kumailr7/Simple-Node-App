420c1578342cdf71f13c16333c7beb295aa1ff44
Kumail7 <57062248+kumailr7@users.noreply.github.com>
2023-02-06 16:19:01 +0530

    updated ip in deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -24,6 +24,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3002 to see your Node.js/React application in action.'
+echo 'Visit http://192.168.58.128:3002 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3002:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
