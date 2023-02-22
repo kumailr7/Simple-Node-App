409019fbfc2619bca243c80cd100bd4cc07cd750
Kumail7 <57062248+kumailr7@users.noreply.github.com>
2023-02-22 16:33:56 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+let assert = require('assert');
 
 describe('Basic Mocha String Test', function () {
  it('should return number of charachters in a string', function () {
